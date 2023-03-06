472a5b577ee4d0a6a6d38cdbe654a0cffa7bd99b
devrobertocf <119601213+devrobertocf@users.noreply.github.com>
2023-03-06 13:41:55 -0300

    unificar js

--- script.js
+++ script.js
@@ -12,6 +12,32 @@ document.addEventListener('DOMContentLoaded', () =>{
     .go() // Faz rodar a animação.
 })
 
+//Mudar a cor do botão quando coloca e tira o mouse.
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+  button.addEventListener('mouseenter', () => {
+    button.style.backgroundColor = '#333';
+    button.style.color = '#fff';
+  });
+
+  button.addEventListener('mouseleave', () => {
+    button.style.backgroundColor = '#da0c0c';
+    button.style.color = '#fff';
+  });
+});
+
+//Deixar texto em NEGRITO quando passa o mouse em cima.
+
+function negrito(elemento) {
+  elemento.style.fontWeight = "bold";
+}
+
+function normal(elemento) {
+  elemento.style.fontWeight = "normal";
+}
+
 //Texto será escrito um depois do outro, depois apaga tudo.
 //document.addEventListener('DOMContentLoaded', () =>{
     //new TypeIt(".animated", {
